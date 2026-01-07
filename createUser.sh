0001c27b4dc68e80c058e44982d9fbf33ad54ae4
TVBChary <tvbchary@xml.com>
2026-01-07 19:02:03 +0530

    Added annotations

--- createUser.sh
+++ createUser.sh
@@ -14,24 +14,31 @@ else
 
 fi
 
+# Check if user created successfully or not (Optional check)
 if ! id "$uname" &>/dev/null; then
 	echo "user $uname does not exist"
 	exit 1
 
 fi
 
+# Get password and confirm password phrases
 read -s -p "Enter New Password: " PASSWORD
 echo
 read -s -p "Confirm new password: " CONFIRM
 echo
 
-
+# Check if passwords match or not
 if [ "$PASSWORD" != "$CONFIRM" ]; then
 	echo "Password do not match"
 	exit1
 
 fi
 
+# Set key value pair and set passowrd using chpasswd
 echo "$uname:$PASSWORD" | sudo chpasswd
 
 echo "Password successfully set for $uname"
+
+################################################
+#                 End of Code                  #
+################################################
